7a8865d929fe4f8b0ca874df632eb1e6039491fb
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-06-29 21:54:48 +0200

    Terminé exercice 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,13 +1,25 @@
-const dummy = (blogs) => {
-  return 1
-}
+const lodash = require('lodash')
+
+const dummy = blogs => 1
 
 const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
 const favoriteBlog = blogs => blogs.reduce((counter, blog) => blog.likes > counter.likes ? blog : counter, blogs[0])
 
+const mostBlogs = blogs => {
+  const groupByAuthor = lodash.groupBy(blogs, 'author')
+  const getCountByAuthor = lodash.map(groupByAuthor, (blogs, author) => {
+    return {
+      author: author,
+      blogs: blogs.length
+    }
+  })
+  return lodash.maxBy(getCountByAuthor, 'blogs')
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
