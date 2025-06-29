f0a639cb43832d286ce742738a65d8c04c07bfc5
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-06-29 17:32:01 +0200

    Terminé exercice 4.2

--- index.js
+++ index.js
@@ -1,47 +1,8 @@
-const express = require('express')
-const mongoose = require('mongoose')
-
+const app = require('./app')
 const config = require('./utils/config')
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(express.json())
-
-// Pour tester si tout fonctionne :
-app.get('/', (req, res) => {
-  res.send('Hello from Node!')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-//TODO : empêcher de renvoyer le champ __v lors des responses
-//       et faire en sorte d'enlever le _ au début du champ 'id'.
+const logger = require('./utils/logger')
 
 const PORT = config.PORT
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
