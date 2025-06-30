2b44b09f5502a6ce3861d051f2130c8a0980fd2f
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-06-30 16:49:33 +0200

    Terminé exercice 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -13,17 +13,16 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-// test('blogs are returned as json', async () => {
-//   await api
-//     .get('/api/blogs')
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/)
-// })
-
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('unique identifier property is named "id"', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert(response.body.every(el => el.id))
 })
 
 after(async () => {
